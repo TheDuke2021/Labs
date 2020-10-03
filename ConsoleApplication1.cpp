2ed5493828d3554f36d1ab088a26d9274234df3a
Дамир <rbk_74@mail.ru>
2020-10-03 19:17:14 +0300

    1

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <math.h>
+const double pi = 3.1415;
+
+
+int main()
+{
+
+	// Различные арифметические операции
+	int alpha = 2 * 2 * 2 * 2 * 2;
+	alpha++;
+	double beta = alpha / 9;
+	double gamma = beta * 3 + alpha / 3;
+	//Тернарная операция
+	alpha % 2 == 0 ? printf("%s", "even") : printf("%s", "odd");
+	beta = -beta;
+	//Условные операторы
+	if (beta >= 0)
+		printf("%s", "+");
+	if (alpha % 2 == 0 && alpha < 0)
+		printf("%s", "YES\n");
+	else
+		printf("%s", "NO\n");
+	//Символьный тип данных
+	char x = 'X';
+	x += 10;
+
+	//Программа по заданным данным находит объем конуса
+	int h;
+	int r;
+	printf("%s", "Input height\n");
+	scanf_s("%d", &h);
+	printf("%s", "and radius: ");
+	scanf_s("%d", &r);
+	double square = pi * r * r * h;
+	int squareInt = (int)square;
+	printf("%s", "square is: ");
+	printf("%d", squareInt);
+	printf("%s", "\n");
+}
