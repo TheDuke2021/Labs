71119f5a2323de551c1a975f0cda8b93b9d0f440
Дамир <rbk_74@mail.ru>
2020-10-09 08:08:49 +0300

    Made labs 5 and 6

--- Lab_5/Lab_5.cpp
+++ Lab_5/Lab_5.cpp
@@ -1,34 +1,110 @@
-﻿
+﻿//ВВП, Лабораторная работа номер 5, Бараев Дамир 201-725
+//Программа написана на языке C++
 
 #include <iostream>
 
-int correct(int* arr, int size);
-void shuffle(int* arr, int size);
-void bogoSort(int* arr, int size);
-
 using namespace std;
+
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+void task_5();
 
 int main()
 {
-    int a[] = {10,9,8,7,6,5,4,3,2,1};
-    bogoSort(a, 10);
-    for (int i = 0; i < 10; i++)
-        cout << a[i] << " ";
+	//Каждый task - это отдельное задание в лабораторной работе
+	//Можно закомментировать нужные строчки, чтобы проверить 
+	//работоспособность отдельно взятого задания
+	task_1();
+	task_2();
+	task_3();
+	task_4();
+	task_5();
 }
 
-int correct(int* arr, int size) {
-    while (size-- > 0)
-        if (arr[size - 1] > arr[size])
-            return 0;
-    return 1;
+void task_1()
+{
+
+	//1. Найти расстояние между двумя точками с заданными координатами (x1, y1) и (x2, y2)
+	int x1, x2, y1, y2;
+	cout << "Type first point's coordinates: " << endl;
+	cin >> x1 >> y1;
+	cout << "\nType second point's coordinates: " << endl;
+	cin >> x2 >> y2;
+	double r = sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
+	cout << "Length is: ";
+	printf_s("%.2lf, %s", r, "\n");
+
 }
 
-void shuffle(int* arr, int size) {
-    for (int i = 0; i < size; ++i)
-        swap(arr[i], arr[(rand() % size)]);
+void task_2()
+{
+
+	//2. Даны три точки A, B, C на числовой оси. Найти длины отрезков AC
+	//   и BC и их сумму.
+	int a, b, c;
+	cout << "Input a, b, c\n";
+	cin >> a >> b >> c;
+	int ac = abs(a - c);
+	int bc = abs(b - c);
+	cout << "\nAC is " << ac;
+	cout << "\nBC is " << bc;
+	cout << "\nAC+BC is " << ac+bc << endl;
+
 }
 
-void bogoSort(int* arr, int size) {
-    while (!correct(arr, size))
-        shuffle(arr, size);
+void task_3()
+{
+	//3. Даны три точки A, B, C на числовой оси. Точка C расположена
+	//   между точками A и B.Найти произведение длин отрезков AC и BC
+	int a, b, c;
+	cout << "Input a, b, c\n";
+	cin >> a >> b >> c;
+	int ac = abs(a - c);
+	int bc = abs(b - c);
+	cout << "\nAC x BC is " << ac*bc << endl;
+}
+
+void task_4()
+{
+	//4. Даны координаты двух противоположных вершин прямоугольника :
+	//   (x1, y1), (x2, y2).Стороны прямоугольника параллельны осям координат.
+	int x1, x2, y1, y2;
+	cout << "Type first point's coordinates: " << endl;
+	cin >> x1 >> y1;
+	cout << "\nType second point's coordinates: " << endl;
+	cin >> x2 >> y2;
+	int length = abs(x1 - x2);
+	int height = abs(y1 - y2);
+	cout << "Perimeter is: " << 2 * (length + height) << endl;
+	cout << "Square is: " << length*height << endl;
+
+}
+
+void task_5()
+{
+	//5. Даны координаты трех вершин треугольника: (x1, y1), (x2, y2), (x3, y3). 
+	//   Найти его периметр и площадь
+	int x1, x2, x3, y1, y2, y3;
+	cout << "Type first point's coordinates: " << endl;
+	cin >> x1 >> y1;
+	cout << "\nType second point's coordinates: " << endl;
+	cin >> x2 >> y2;
+	cout << "\nType third point's coordinates: " << endl;
+	cin >> x3 >> y3;
+	//Находим длину каждой стороны
+	double AB = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	double BC = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+	double AC = sqrt(pow(x2 - x3, 2) + pow(y2 - y3, 2));
+	//Проверяем, существует ли такой треугольник
+	if (AC >= AB+BC || AB >= AC+BC || BC >= AC+AB) {
+		cout << "Да что вы говорите" << endl;
+		return;
+	}
+	//Используем формулу Герона
+	double p = (AB + BC + AC)/2;
+	double sq = sqrt(p*(p-AB)*(p-BC)*(p-AC));
+	cout << "Perimeter is: " << p*2 << endl;
+	cout << "Square is: " << sq<< endl;
 }
---
+++ Lab_6/Lab_6.cpp
@@ -0,0 +1,130 @@
+﻿//ВВП, Лабораторная работа номер 6, Бараев Дамир 201-725
+//Программа написана на языке C++
+
+#include <iostream>
+
+using namespace std;
+
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+void task_5();
+void task_6();
+void task_7();
+
+int main()
+{
+	//Каждый task - это отдельное задание в лабораторной работе
+	//Можно закомментировать нужные строчки, чтобы проверить 
+	//работоспособность отдельно взятого задания
+	//task_1();
+	//task_2();
+	//task_3();
+	//task_4();
+	//task_5();
+	//task_6();
+	task_7();
+}
+
+void task_1()
+{
+
+	//1. Поменять местами содержимое переменных A и B и вывести новые значения A и B.
+	int a, b;
+	cout << "Input a and b: " << endl;
+	cin >> a >> b;
+	int temp = a;
+	a = b;
+	b = temp;
+	cout << "\n a is: " << a << endl;
+	cout << "\n b is: " << b << endl;
+
+}
+
+void task_2()
+{
+
+	//2. Даны переменные A, B, C. Изменить их значения, 
+	//   переместив содержимое A в B, B — в C, C — в A, и вывести 
+	//   новые значения переменных A, B, C.
+	int a, b, c;
+	cout << "Input a, b, c\n";
+	cin >> a >> b >> c;
+	int temp = a;
+	a = c;
+	c = b;
+	b = temp;
+	cout << "\nA is " << a;
+	cout << "\nB is " << b;
+	cout << "\nC is " << c << endl;
+
+}
+
+void task_3()
+{
+	//3. Даны переменные A, B, C. Изменить их значения, 
+	//   переместив содержимое A в C, C — в B, B — в A, и вывести новые 
+	//   значения переменных A, B, C.
+	int a, b, c;
+	cout << "\nInput a, b, c\n";
+	cin >> a >> b >> c;
+	int temp = c;
+	c = a;
+	a = b;
+	b = temp;
+	cout << "\nA is " << a;
+	cout << "\nB is " << b;
+	cout << "\nC is " << c << endl;
+}
+
+void task_4()
+{
+	//4. Найти значение функции y = 3x^6 − 6x^2 − 7 при данном значении x
+	double x;
+	cout << "\nInput x: " << endl;
+	cin >> x;
+	double y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+	cout << "\ny is: " << y << endl;
+
+}
+
+void task_5()
+{
+	//5. Найти значение функции y = 4(x−3)^6 − 7(x−3)^3 + 2 при данном значении x
+	double x;
+	cout << "\nInput x: " << endl;
+	cin >> x;
+	double y = 4 * pow(x-3, 6) - 7 * pow(x-3, 3) + 2;
+	cout << "\ny is: " << y << endl;
+}
+
+void task_6()
+{
+	//6. Дано число A. Вычислить A^8 , используя вспомогательную 
+	//   переменную и три операции умножения.
+	double A;
+	cout << "\nInput A: " << endl;
+	cin >> A;
+	double A2 = A * A;
+	A2 = A2 * A2;
+	A2 = A2 * A2;
+	cout << "\nA is: " << A2 << endl;
+
+}
+
+void task_7()
+{
+
+	//7. Дано число A. Вычислить A^15, используя две вспомогательные 
+	//   переменные и пять операций умножения.
+	double A;
+	cout << "\nInput A: " << endl;
+	cin >> A;
+	double A2 = A * A * A;
+	double A3 = A2 * A2;
+	A3 = A3 * A3;
+	A3 = A3 * A2;
+	cout << "\nA is: " << A3 << endl;
+
+}
